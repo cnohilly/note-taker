44ab1b06685d87359e76ac9fd62c02438df7100e
ctnohilly <ctnohilly@gmail.com>
2022-07-16 15:03:32 -0400

    api changes and verifying that db data is not empty

--- server.js
+++ server.js
@@ -13,34 +13,66 @@ app.use(express.static('public'));
 
 // home page for application
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // page for the notes being taken or saved
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req,res) => {
-    fs.readFile('./db/db.json','utf8', (err,data) => {
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
-        res.json(JSON.parse(data));
+        if (!data){
+            data = [];
+        } else {
+            data = JSON.parse(data);
+        }
+        res.json(data);
     })
 });
 
-app.post('/api/notes', (req,res) => {
-    fs.readFile('./db/db.json','utf-8', (err,data) => {
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) throw err;
-        data = JSON.parse(data);
+        if (!data){
+            data = [];
+        } else {
+            data = JSON.parse(data);
+        }
         data.push(req.body);
-        fs.writeFile('./db/db.json',JSON.stringify(data), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(data), (err) => {
             if (err) throw err;
         });
         res.json(req.body);
     });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        if (!data){
+            data = [];
+        } else {
+            data = JSON.parse(data);
+        }
+        const noteId = req.params.id;
+        console.log(noteId);
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id == noteId) {
+                data.splice(i, 1);
+                fs.writeFile('./db/db.json', JSON.stringify(data), (err) => {
+                    if (err) throw err;
+                    console.log('Item removed and database saved.');
+                });
+                res.json('Note has been deleted');
+            }
+        }
+    })
+});
+
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
